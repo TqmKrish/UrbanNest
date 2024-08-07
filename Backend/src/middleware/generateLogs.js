97c844b82404565f5aa7b7a98dc5b5f0769ec95e
TqmKrish <gkrish@teqmavens.com>
2024-08-07 18:57:18 +0530

    minor fix

--- Backend/src/middleware/generateLogs.js
+++ Backend/src/middleware/generateLogs.js
@@ -27,3 +27,38 @@ const generateLogReqRes = () => {
 };
 
 module.exports = generateLogReqRes;
+
+// const fs = require("fs");
+
+// const generateLogReqRes = (fileName) => {
+//   return (req, res, next) => {
+//     const originalSend = res.send;
+//     let responseBody;
+
+//     res.send = function (body) {
+//       responseBody = body;
+//       return originalSend.apply(this, arguments);
+//     };
+
+//     res.on("finish", () => {
+//       const logEntry = `
+//   Time: ${new Date().toISOString()}
+//   IP: ${req.ip}
+//   Method: ${req.method}
+//   Path: ${req.path}
+//   Request Body: ${JSON.stringify(req.body, null, 2)}
+//   Response Body: ${JSON.stringify(JSON.parse(responseBody), null, 2)}
+// `;
+
+//       fs.appendFile(fileName, logEntry, (error) => {
+//         if (error) {
+//           console.error("Error writing log:", error);
+//         }
+//       });
+//     });
+
+//     next();
+//   };
+// };
+
+// module.exports = generateLogReqRes;
